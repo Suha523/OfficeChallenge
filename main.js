ed99460b0574d3e83c053cdd240219ac938d327f
suha <suhashehahde@gmail.com>
2022-07-17 14:44:46 -0700

    add my oop inrto exercise

--- main.js
+++ main.js
@@ -1,2 +1,74 @@
-//write your code here to make the tests pass
+class Document {
+  constructor(EmployeeName) {
+    this.EmployeeName = EmployeeName;
+  }
+}
 
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+
+  work(office) {
+    for (let i = 1; i <= 10; i++) {
+      let document = new Document(this.name);
+      office.documents.push(document);
+    }
+  }
+}
+
+class Manager {
+  constructor(name) {
+    this.name = name;
+    this.employees = [];
+  }
+
+  hireEmployee(name) {
+    let employee = new Employee(name);
+    this.employees.push(employee);
+  }
+
+  askEmployeesToWork(office) {
+    for (let employee of this.employees) {
+      employee.work(office);
+    }
+  }
+}
+
+class Cleaner {
+  constructor(name) {
+    this.name = name;
+  }
+
+  clean() {
+    console.log("Clean");
+  }
+}
+
+class Office {
+  constructor() {
+    this.documents = [];
+    this.managers = [];
+    this.cleaners = [];
+  }
+
+  hireCleaner(name) {
+    let cleaner = new Cleaner(name);
+    this.cleaners.push(cleaner);
+  }
+
+  hireManager(name) {
+    let manager = new Manager(name);
+    this.managers.push(manager);
+  }
+
+  startWorkDay() {
+    for (let manager of this.managers) {
+      manager.askEmployeesToWork(this);
+    }
+
+    for (let cleaner of this.cleaners) {
+      cleaner.clean();
+    }
+  }
+}
